c271dd8e17ba3269ea7f9659f9b1b34683057ea8
marcos <marcos.hardy@outlook.com>
2024-10-16 22:34:24 +0100

    chore: refactor code and validate input

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,22 @@
+import { processInput } from "./helpers";
 import RoverSquad from "./RoverSquad";
-import { RoverData } from "./types";
+import { RoverPosition } from "./types";
 
-export const processInput = (input: string[]) => {
-  // The first line of input is the upper-right coordinates of the plateau
-  const [plateauWidth, plateauHeight] = input[0].split(" ").map((value) => parseInt(value));
+export const processAndOutputRovers = (input: string[]): RoverPosition[] => {
+  const { plateauWidth, plateauHeight, roversData } = processInput(input);
 
-  // Create a new RoverSquad instance with the plateau size
+  // Create a new RoverSquad instance with the plateau dimensions
   const roverSquad = new RoverSquad({ width: plateauWidth, height: plateauHeight });
 
-  /*
-   The first line of the input is the plateau coordinates so we need to start with index 1
-   Each rover has two lines of input so we want to increment index by 2 
-   This insures each iteration we get the rovers coordinates and series of commands for each rover
-  */
+  const finalPositions = roverSquad.deployRovers(roversData);
 
-  const roversData: RoverData[] = [];
-
-  for (let i = 1; i < input.length; i += 2) {
-    // rover's position e.g 1 2 N
-    const position = input[i];
-    // series of instructions telling the rover how to explore the plateau e.g LMLMLMLMM
-    const instructions = input[i + 1];
-
-    roversData.push({ position, instructions });
-  }
-
-  return roverSquad.deployRovers(roversData).map((roverPosition) => roverPosition);
+  return finalPositions;
 };
 
-const result = processInput(["5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"]);
+// Example input
+const input = ["5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"];
+
+// Process input and output results and receive an array of Rover positions
+const result = processAndOutputRovers(input);
 
 console.log("result:", result);
